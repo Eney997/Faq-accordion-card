dcc13285ff5f9bbf88574ecdf423619b2a034dc7
Tornike Dzidziguri <tornikedzidziguri2001@gmail.com>
2023-11-15 12:29:42 +0400

    done xar done

--- app.js
+++ app.js
@@ -1,36 +1,9 @@
 const yvelah = document.querySelector("#hebisid")
 const yvelap = document.querySelector("#pforjs")
+const arrow = document.querySelector("#arrowid")
 
 
-
-for (let i = 0; i < yvelah.length; i++) {
-    yvelah[i].addEventListener('click',()=>{
-        yvelap[i].classList.toggle('active');
-
-        for (let j = 0; c < yvelap.length; j++) {
-            if (j !== i) {
-                yvelap[c].classList.remove('active');
-            }
-        }
-    });
-}
-
-
-
-
-
-
-
-// yvelah.addEventListener("click",()=>{
-//     yvelap.classList.toggle("active")
-// })
-
-
-
-
-
-
-
-
-
-
+yvelah.addEventListener("click",()=>{
+    yvelap.classList.toggle("active")
+    arrow.classList.toggle("rotate")
+})
